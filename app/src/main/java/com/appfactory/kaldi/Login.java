d49d7e74f56e5db8677e3dafd9ad19267c2992dd
ophir rahimi <orahimi@usc.edu>
2019-11-01 16:06:10 -0700

    checking how to transfer between pages

---
+++ app/src/main/java/com/appfactory/kaldi/Login.java
@@ -0,0 +1,27 @@
+package com.appfactory.kaldi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        Button button1 =  (Button) findViewById(R.id.loginButton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Registration_student.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+    }
+
+}
