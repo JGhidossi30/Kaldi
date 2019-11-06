31f2ab267d9d77e97f59d62d9efbb26bc1e7c313
Derek <derekye@usc.edu>
2019-11-05 21:27:45 -0800

    Drinker main and corresponding pages created

---
+++ app/src/main/java/com/appfactory/kaldi/Drinker_Main.java
@@ -0,0 +1,48 @@
+package com.appfactory.kaldi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Drinker_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drinker__main);
+
+        Button manageProfile =  (Button) findViewById(R.id.manageProfile);
+        manageProfile.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                Intent myIntent = new Intent(view.getContext(), Manage_Profile.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+        Button historyButton =  (Button) findViewById(R.id.history);
+        manageProfile.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                Intent myIntent = new Intent(view.getContext(), Order_History.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+        Button mapButton =  (Button) findViewById(R.id.mapButton);
+        manageProfile.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                Intent myIntent = new Intent(view.getContext(), Login.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+    }
+}
---
+++ app/src/main/java/com/appfactory/kaldi/Manage_Profile.java
@@ -0,0 +1,14 @@
+package com.appfactory.kaldi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Manage_Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage__profile);
+    }
+}
