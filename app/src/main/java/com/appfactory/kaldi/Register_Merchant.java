af66302dc4a9f746a75223b835012a1a45e98156
Jordan Ghidossi <ghidossi@usc.edu>
2019-11-05 17:21:58 -0800

    Update Register_Merchant.java
    Update merchant class

--- app/src/main/java/com/appfactory/kaldi/Register_Merchant.java
+++ app/src/main/java/com/appfactory/kaldi/Register_Merchant.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Register_Merchant extends AppCompatActivity
 {
@@ -28,11 +31,7 @@ public class Register_Merchant extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                String storeName = findViewById(R.id.storeInput).toString();
-                String nameInput = findViewById(R.id.adminInput1).toString();
-                String emailInput = findViewById(R.id.emailInput).toString();
-                String passwordInput = findViewById(R.id.passwordInput).toString();
-                String confirmPasswordInput = findViewById(R.id.confirmPasswordInput).toString();
+
                 //check for  email and password (these two are done for student already)
                 //deal with storename after
                 //create page following registration of merchant for address, then add page
@@ -40,14 +39,48 @@ public class Register_Merchant extends AppCompatActivity
                 // after this, store in DB and finish registration, following the clicking of
                 //"finish registration" we will go to their own storefront page
 
-                Merchant business = new Merchant(nameInput, passwordInput, emailInput, "Store Name", new Location(0, 0), new Menu());
 
-//                we need to now go to a new page once registered
-                Intent myIntent = new Intent(view.getContext(), Register.class);
-                startActivity(myIntent);
+
+
+                TextView storeNameInput = findViewById(R.id.storeInput);
+                TextView nameInput = findViewById(R.id.adminInput1);
+                TextView emailInput = findViewById(R.id.emailInput);
+                TextView passwordInput = findViewById(R.id.passwordInput);
+                TextView confirmPasswordInput = findViewById(R.id.confirmPasswordInput);
+                TextView addressInput = findViewById(R.id.addressInput);
+                TextView initialItemInput = findViewById(R.id.initialItemInput);
+                TextView caffeineInput = findViewById(R.id.caffeineInput);
+
+                String storeName = nameInput.getText().toString();
+                String name = nameInput.getText().toString();
+                String email = emailInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                String confirmPassword = confirmPasswordInput.getText().toString();
+                String address = addressInput.getText().toString();
+                String initialItem = initialItemInput.getText().toString();
+                int caffine = Integer.getInteger(caffeineInput.getText().toString());
+
+                if (!password.equals(confirmPassword))
+                {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Passwords do not match!", Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
+                    toast.show();
+                }
+                else if (Merchant.exists(email))
+                {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Email already exists!", Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
+                    toast.show();
+                }
+                else
+                {
+                    new Merchant(name, password, email, storeName, address, new Menu(new Item(initialItem, caffeine)));
+
+                    //Update Page
+                    Intent myIntent = new Intent(view.getContext(), Register.class);
+                    startActivity(myIntent);
+                }
             }
-
         });
     }
-
 }
