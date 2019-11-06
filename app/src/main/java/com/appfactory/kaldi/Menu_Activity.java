1044159a0aa0037c0de2ee42178c2aa261b3f450
Derek <derekye@usc.edu>
2019-11-06 01:46:10 -0800

    poopoo

---
+++ app/src/main/java/com/appfactory/kaldi/Menu_Activity.java
@@ -0,0 +1,72 @@
+package com.appfactory.kaldi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class Menu_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        ArrayList<String> bag = new ArrayList<String>();
+
+        Button icedCoffee =  (Button) findViewById(R.id.iced_coffee);
+        icedCoffee.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view) {
+                bag.add("Iced Coffee");
+            }
+
+        });
+        Button americano =  (Button) findViewById(R.id.americano);
+        americano.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view) {
+                bag.add("Americano");
+            }
+
+        });
+        Button chaiTea =  (Button) findViewById(R.id.chai_tea);
+        chaiTea.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view) {
+                bag.add("Chai Tea Latte");
+            }
+
+        });
+        Button greenTea =  (Button) findViewById(R.id.green_tea);
+        greenTea.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view) {
+                bag.add("Green Tea");
+            }
+
+        });
+        Button blackTea =  (Button) findViewById(R.id.black_tea);
+        blackTea.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view) {
+                bag.add("Black Tea");
+            }
+        });
+        Button checkout =  (Button) findViewById(R.id.checkout);
+        checkout.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                Intent myIntent = new Intent(view.getContext(), Checkout.class);
+                myIntent.putExtra("BAG", bag);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+    }
+}
