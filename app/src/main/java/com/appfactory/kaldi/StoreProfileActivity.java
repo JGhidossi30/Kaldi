0b5f820a74b1a0d18e061adb549c9bd6315db16e
Aron Vischjager <vischjag@usc.edu>
2019-11-24 21:24:10 -0800

    Adding business file

--- app/src/main/java/com/appfactory/kaldi/StoreProfileActivity.java
+++ app/src/main/java/com/appfactory/kaldi/StoreProfileActivity.java
@@ -1,14 +1,18 @@
 package com.appfactory.kaldi;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class StoreProfileActivity extends AppCompatActivity
 {
+    Intent myFileIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -34,8 +38,26 @@ public class StoreProfileActivity extends AppCompatActivity
             {
                 String currentUser = getIntent().getStringExtra("currentUser");
                 boolean isDrinker = getIntent().getBooleanExtra("isDrinker", true);
+                myFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                myFileIntent.setType("*/*");
+                startActivityForResult(myFileIntent, 10);
+
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 10:
+                if((resultCode == RESULT_OK) && (data != null))
+                {
+                    String path = data.getData().getPath();
+                    Log.d("path", "" + path);
+                }
+                break;
+        }
+    }
 }
