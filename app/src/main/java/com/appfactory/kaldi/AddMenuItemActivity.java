ae8cbb7f5bec99e6e6d90332de1030108ebff429
Aron Vischjager <vischjag@usc.edu>
2019-11-24 21:29:46 -0800

    Update menu item page

--- app/src/main/java/com/appfactory/kaldi/AddMenuItemActivity.java
+++ app/src/main/java/com/appfactory/kaldi/AddMenuItemActivity.java
@@ -25,79 +25,79 @@ public class AddMenuItemActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_menu_item);
-
-        TextView businessNameInput = (TextView) findViewById(R.id.businessName);
-        TextView initialItemInput = (TextView) findViewById(R.id.initialItem);
-        TextView caffeineInput = (TextView) findViewById(R.id.caffeine);
-
-        String businessName = businessNameInput.getText().toString();
-        String initialItem = initialItemInput.getText().toString();
-        String caffeine = caffeineInput.getText().toString();
-        Button addMenuItem =  (Button) findViewById(R.id.addMenuItem);
-        addMenuItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DatabaseReference database = FirebaseDatabase.getInstance().getReference("users").child("merchants");
-                Query search;
-//                if (getIntent().getBooleanExtra("isDrinker", true))
-//                    search = database.child("drinkers").orderByKey();
-//                else
-                    search = database.child("merchants").orderByKey();
-                search.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                            if (snapshot.getKey().equals(getIntent().getStringExtra("currentUser"))) {
-                                Merchant merchant;
-//                                if (getIntent().getBooleanExtra("isDrinker", true))
-//                                    drinker = snapshot.getValue(Drinker.class);
-//                                else
-                                    merchant = snapshot.getValue(Merchant.class);
-                                merchant.id = snapshot.getKey();
-                                for (int i = 0; i < merchant.stores.size(); i++)
-                                {
-                                    if (merchant.stores.get(i).storeName.equals(businessName))
-                                    {
-                                        merchant.stores.get(i).menu.addItem(new Item(initialItem, Integer.parseInt(caffeine)));
-                                    }
-                                }
-                                merchant.submitToDatabase();
-
-                                //Intent myIntent = new Intent(view.getContext(), CheckoutActivity.class); Might want to bring this page back
-
-//                                if (getIntent().getBooleanExtra("isDrinker", true))
-//                                    myIntent = new Intent(view.getContext(), DrinkerMainActivity.class);
-//                                else
-                                Intent myIntent = myIntent = new Intent(getApplicationContext(), MerchantMainActivity.class);
-                                String currentUser = getIntent().getStringExtra("currentUser");
-                                boolean isDrinker = getIntent().getBooleanExtra("isDrinker", true);
-                                myIntent.putExtra("currentUser", currentUser);
-                                myIntent.putExtra("isDrinker", isDrinker);
-                                startActivityForResult(myIntent, 0);
-
-                                Toast toast = Toast.makeText(getApplicationContext(), "New Business Added!", Toast.LENGTH_LONG);
-                                toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
-                                toast.show();
-                                break;
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
-            }
-        });
+//
+//        TextView businessNameInput = (TextView) findViewById(R.id.businessName);
+//        TextView initialItemInput = (TextView) findViewById(R.id.initialItem);
+//        TextView caffeineInput = (TextView) findViewById(R.id.caffeine);
+//
+//        String businessName = businessNameInput.getText().toString();
+//        String initialItem = initialItemInput.getText().toString();
+//        String caffeine = caffeineInput.getText().toString();
+//        Button addMenuItem =  (Button) findViewById(R.id.addMenuItem);
+//        addMenuItem.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                DatabaseReference database = FirebaseDatabase.getInstance().getReference("users").child("merchants");
+//                Query search;
+////                if (getIntent().getBooleanExtra("isDrinker", true))
+////                    search = database.child("drinkers").orderByKey();
+////                else
+//                    search = database.child("merchants").orderByKey();
+//                search.addListenerForSingleValueEvent(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+//                            if (snapshot.getKey().equals(getIntent().getStringExtra("currentUser"))) {
+//                                Merchant merchant;
+////                                if (getIntent().getBooleanExtra("isDrinker", true))
+////                                    drinker = snapshot.getValue(Drinker.class);
+////                                else
+//                                    merchant = snapshot.getValue(Merchant.class);
+//                                merchant.id = snapshot.getKey();
+//                                for (int i = 0; i < merchant.stores.size(); i++)
+//                                {
+//                                    if (merchant.stores.get(i).storeName.equals(businessName))
+//                                    {
+//                                        merchant.stores.get(i).menu.addItem(new Item(initialItem, Integer.parseInt(caffeine)));
+//                                    }
+//                                }
+//                                merchant.submitToDatabase();
+//
+//                                //Intent myIntent = new Intent(view.getContext(), CheckoutActivity.class); Might want to bring this page back
+//
+////                                if (getIntent().getBooleanExtra("isDrinker", true))
+////                                    myIntent = new Intent(view.getContext(), DrinkerMainActivity.class);
+////                                else
+//                                Intent myIntent = myIntent = new Intent(getApplicationContext(), MerchantMainActivity.class);
+//                                String currentUser = getIntent().getStringExtra("currentUser");
+//                                boolean isDrinker = getIntent().getBooleanExtra("isDrinker", true);
+//                                myIntent.putExtra("currentUser", currentUser);
+//                                myIntent.putExtra("isDrinker", isDrinker);
+//                                startActivityForResult(myIntent, 0);
+//
+//                                Toast toast = Toast.makeText(getApplicationContext(), "New Business Added!", Toast.LENGTH_LONG);
+//                                toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+//                                toast.show();
+//                                break;
+//                            }
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//                    }
+//                });
+//            }
+//        });
     }
 
-    /**
-     *
-     */
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
-    }
+//    /**
+//     *
+//     */
+//    @Override
+//    protected void onStart()
+//    {
+//        super.onStart();
+//    }
 }
