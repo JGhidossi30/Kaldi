6d7f9bd3dea182fd3a717f5c3b0bf25146e8ce5c
Derek <derekye@usc.edu>
2019-11-24 22:01:10 -0800

    blackbox

---
+++ app/src/androidTest/java/com/appfactory/kaldi/WhiteBoxMainActivityTest.java
@@ -0,0 +1,20 @@
+package com.appfactory.kaldi;
+
+//import com.google.common.truth.Truth.assertThat;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class WhiteBoxMainActivityTest {
+    @Test
+    public void emailValidatorReturnsFalse() {
+        boolean value = MainActivity.validateEmail("thisisthewrong.com");
+        assertEquals(false, value);
+    }
+    @Test
+    public void emailValidatorReturnsTrue() {
+        boolean value = MainActivity.validateEmail("thisisright@email.com");
+        assertEquals(true, value);
+    }
+}
