9c40f99a39dc62ac0d978a7522739cd070734b68
Jordan Ghidossi <ghidossi@usc.edu>
2019-12-10 01:41:11 -0800

    RemoveMenuItem

--- app/src/main/java/com/appfactory/kaldi/RemoveMenuItemActivity.java
+++ app/src/main/java/com/appfactory/kaldi/RemoveMenuItemActivity.java
@@ -1,96 +1,96 @@
 package com.appfactory.kaldi;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 public class RemoveMenuItemActivity extends AppCompatActivity {
+
+    private Button newStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remove_menu_item);
-        TextView itemInput = (TextView) findViewById(R.id.itemName);
-        TextView caffeineInput = (TextView) findViewById(R.id.caffeine);
-        TextView priceInput = (TextView) findViewById(R.id.price);
-        Button addMenuItem =  (Button) findViewById(R.id.addButton);
-        addMenuItem.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                String businessName = getIntent().getStringExtra("storeName");
-                String userName = CurrentUser.getInstance().getId();
-                Log.d("user", "" + userName);
-                String itemName = itemInput.getText().toString();
-                int caffeine = Integer.parseInt(caffeineInput.getText().toString());
-                double price = Double.parseDouble(priceInput.getText().toString());
-                DatabaseReference database = FirebaseDatabase.getInstance().getReference("users");
-                Query search = database.child("merchants").orderByKey().equalTo(userName);
-                search.addListenerForSingleValueEvent(new ValueEventListener()
-                {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot)
-                    {
-                        for (DataSnapshot snapshot : dataSnapshot.getChildren())
-                        {
-                            if ((snapshot.getKey() != null) && (snapshot.getKey().equals(userName)))
-                            {
-                                Merchant merchant = snapshot.getValue(Merchant.class);
-                                if (merchant != null)
-                                {
-                                    for (int i = 0; i < merchant.stores.size(); i++)
-                                    {
-                                        if (merchant.stores.get(i).storeName.equals(businessName))
-                                        {
-                                            merchant.id = snapshot.getKey();
-                                            merchant.stores.get(i).menu.add(new Item(itemName, caffeine, price));
-                                            merchant.submitToDatabase();
-                                        }
-                                        break;
-                                    }
-                                    Intent myIntent = myIntent = new Intent(getApplicationContext(), StoreProfileActivity.class);
-                                    startActivityForResult(myIntent, 0);
-                                    Toast toast = Toast.makeText(getApplicationContext(), "New item Added!", Toast.LENGTH_LONG);
-                                    toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
-                                    toast.show();
-                                    break;
-                                }
-                            }
-                        }
-                    }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
-            }
-        });
+        getStoreMenu();
     }
 
-    /**
-     *
-     */
-    @Override
-    protected void onStart()
+    public void getStoreMenu()
     {
-        super.onStart();
+        for (Merchant merchant : Database.getInstance().getMerchants())
+        {
+            if (CurrentUser.getInstance().getId().equals(merchant.id))
+            {
+                for (int i = 0; i < merchant.stores.size(); i++)
+                {
+                    if (merchant.stores.get(i).getStoreName().equals(getIntent().getStringExtra("businessTitle")))
+                    {
+                        for (int j = 0; j < merchant.stores.get(i).menu.size(); j++) {
+                            addMenuItem(merchant.stores.get(i).menu.get(j), i, j);
+                        }
+                    }
+                }
+            }
+        }
+//        DatabaseReference database = FirebaseDatabase.getInstance().getReference("users").child("merchants");
+//        database.addValueEventListener(new ValueEventListener()
+//        {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+//            {
+//                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+//                    Merchant merchant = postSnapshot.getValue(Merchant.class);
+//                    if ((merchant != null) && CurrentUser.getInstance().getStores().get(i).equals()CurrentUser.getInstance().getId().equals(merchant.id)) {
+//                        if (merchant.stores != null) {
+//                            for (int i = 0; i < merchant.stores.size(); i++)
+//                            {
+//                                addMenuItem(merchant.stores.get(i), i);
+//                            }
+//                        }
+//                        else {
+//                            Toast toast = Toast.makeText(getApplicationContext(), "Merchant has no stores!", Toast.LENGTH_LONG);
+//                            toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+//                            toast.show();
+//                        }
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+    }
+
+    public void addMenuItem(Item item, int i, int j)
+    {
+        LinearLayout layout = (LinearLayout) findViewById(R.id.rootLayout);
+        String itemContent = item.name;
+        newStore = new Button(this);
+        newStore.setText(itemContent);
+        newStore.setBackgroundColor(Color.WHITE);
+        layout.addView(newStore);
+        newStore.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                DatabaseReference database = FirebaseDatabase.getInstance().getReference("users").child("merchants");
+                database.child(CurrentUser.getInstance().getId()).child("stores").child(Integer.toString(i)).child("menu").child(Integer.toString(j)).removeValue();
+
+                Intent myIntent = new Intent(view.getContext(), RemoveMenuItemActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
     }
 }
